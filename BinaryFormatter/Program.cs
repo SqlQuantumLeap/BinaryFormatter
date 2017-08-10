3a52c28d00d42f655286a95b9e1790c98d446495
Sql Quantum Leap <SqlQuantumLeap@users.noreply.github.com>
2017-08-10 17:56:42 -0400

    Format binary file into hex string file
    Simple command-line utility that reads in a binary file and writes out a
    text file containing the hex representation of the source bytes.
    
    A feature / benefit of BinaryFormatter is that it splits the hex string
    across N lines of "ChunkSize" bytes each, with all lines prior to the
    final line being appended with the T-SQL line-continuation character: /

---
+++ BinaryFormatter/Program.cs
@@ -0,0 +1,137 @@
+﻿/*
+ * BinaryFormatter
+ * Version 1.0.1
+ * Copyright (c) 2009-2017 Solomon Rutzky. All rights reserved.
+ *
+ * Blog: https://SqlQuantumLeap.com/
+ *
+ */
+using System;
+using System.Text;
+using System.IO;
+
+namespace BinaryFormatter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+            if (args.Length < 2)
+            {
+                DisplayUsage();
+
+                return;
+            }
+
+            int _ChunkSize = 10000;
+			string _FilePath = String.Empty;
+			string _OutFile = String.Empty;
+
+			foreach (string _Arg in args)
+			{
+				int _TempSize;
+
+				if (int.TryParse(_Arg, out _TempSize))
+				{
+					_ChunkSize = _TempSize;
+				}
+				else
+				{
+					if (_FilePath == string.Empty)
+					{
+						_FilePath = _Arg;
+					}
+					else if (_OutFile == string.Empty)
+					{
+						_OutFile = _Arg;
+					}
+				}
+			}
+
+			if (_FilePath == string.Empty)
+			{
+                throw new ArgumentNullException("You must specify a source file.");
+			}
+			if (_OutFile == string.Empty)
+			{
+				_OutFile = @".\Assembly.sql";
+			}
+
+			FileStream _FileIn = null;
+			BinaryReader _DataIn = null;
+			FileStream _FileOut = null;
+			StreamWriter _DataOut = null;
+
+			try
+			{
+				using (_FileIn = new FileStream(_FilePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+				{
+					using (_DataIn = new BinaryReader(_FileIn))
+					{
+
+						using (_FileOut = new FileStream(_OutFile, FileMode.Create, FileAccess.Write, FileShare.Read))
+						{
+							using (_DataOut = new StreamWriter(_FileOut, Encoding.ASCII))
+							{
+								_DataIn.BaseStream.Position = 0;
+
+								byte[] _Bytes = new byte[_ChunkSize];
+
+								int _ReadAmount = 0;
+
+								while ((_ReadAmount = _DataIn.Read(_Bytes, 0, _ChunkSize)) > 0)
+								{
+									for (int _Index = 0; _Index < _ReadAmount; _Index++)
+									{
+										_DataOut.Write(_Bytes[_Index].ToString("X2"));
+									}
+
+									if (_DataIn.BaseStream.Position < _DataIn.BaseStream.Length)
+									{
+										_DataOut.WriteLine(@"\");
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+			catch (Exception _Exception)
+			{
+				System.Console.WriteLine(_Exception.Message);
+                System.Console.WriteLine("\n\tPress the <any> key to continue...\n");
+				System.Console.ReadLine();
+			}
+			finally
+			{
+				_DataOut.Dispose();
+				_FileOut.Dispose();
+				_DataIn.Dispose();
+				_FileIn.Dispose();
+			}
+
+			
+		}
+
+        private static void DisplayUsage()
+        {
+            System.Console.WriteLine("\nBinaryFormatter");
+            System.Console.WriteLine("Version 1.0.1");
+            System.Console.WriteLine("Copyright (c) 2009-2017 Solomon Rutzky. All rights reserved.\n");
+            System.Console.WriteLine("https://SqlQuantumLeap.com/\n");
+
+            System.Console.WriteLine("Format binary file into hex string for SQL script.");
+            System.Console.WriteLine("Output broken into multiple lines of \"ChunkSize\" bytes.");
+            System.Console.WriteLine("If there are multiple lines, all lines except the final");
+            System.Console.WriteLine("line are appended with the line-continuation character of: \\");
+
+            System.Console.WriteLine("\nUsage:");
+            System.Console.WriteLine("\n\tBinaryFormatter path\\to\\binary_file.ext path\\to\\Output.sql [ ChunkSize ]");
+            System.Console.WriteLine("\n\tChunkSize = the number of bytes per row. A byte is 2 characters: 00 - FF.");
+            System.Console.WriteLine("\tMaximum line length = (ChunkSize * 2) + 1.");
+            System.Console.WriteLine("\tDefault ChunkSize = 10000\n");
+            return;
+        }
+    }
+
+}
---
+++ BinaryFormatter/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BinaryFormatter")]
+[assembly: AssemblyDescription("Format binary file into hex string for SQL script.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Solomon Rutzky")]
+[assembly: AssemblyProduct("BinaryFormatter")]
+[assembly: AssemblyCopyright("Copyright © 2009-2017 Solomon Rutzky. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("74a1e9c4-ed9b-4f12-93f7-ad7d1ee02a2b")]
+
+
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
